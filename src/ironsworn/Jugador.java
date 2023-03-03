9e9d7db1137ccaeb292f34eb315ddae74537b6dd
yuripastorovic <116525569+yuripastorovic@users.noreply.github.com>
2023-03-03 01:20:59 +0100

    -Primer comit

---
+++ src/ironsworn/Jugador.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ironsworn;
+
+/**
+ *
+ * @author @yuripastorovic
+ */
+public abstract class Jugador {
+    private String nombre;
+    private String personaje;
+    private int filo;
+    private int corazon;
+    private int hierro;
+    private int sombra;
+    private int sabiduria;
+    
+    
+}
